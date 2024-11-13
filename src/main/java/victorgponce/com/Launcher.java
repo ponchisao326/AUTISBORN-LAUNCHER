ba72e14d4ee99d393e5d64bfe7935ea771ac9dda
ponchisao326_ <ponchisao326@hotmail.com>
2024-11-13 23:01:20 +0100

    Created the game folder path

---
+++ src/main/java/victorgponce/com/Launcher.java
@@ -0,0 +1,13 @@
+package victorgponce.com;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+}
--- src/main/java/victorgponce/com/Main.java
+++ src/main/java/victorgponce/com/Main.java
@@ -1,7 +1,21 @@
 package victorgponce.com;
 
+import javafx.application.Application;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Class.forName("javafx.application.Application");
+            Application.launch(Launcher.class, args);
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Error: \n" + e.getMessage() + "not found!",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
     }
 }
